ed2337ccc2ace74e352d6e5c1a69c8bee02a4926
dev-just-iceberg <gentoo.sigma@protonmail.com>
2023-12-08 00:57:38 +0100

    feat: show cat image on todo info
    added a little fetch to get a cat image
    and put it as the body of the modal of
    the todo info and save it in localstorage too

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,31 +1,66 @@
 import React, { useState, useEffect } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const Todo = () => {
   const initialTodos = JSON.parse(localStorage.getItem('todos')) || [];
   const [todos, setTodos] = useState(initialTodos);
   const [task, setTask] = useState('');
+  const [selectedTodo, setSelectedTodo] = useState(null);
 
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos]);
 
-  const addTodo = () => {
+  const addTodo = async () => {
     if (task.trim() !== '') {
       setTodos([...todos, { text: task, completed: false }]);
       setTask('');
     }
   };
 
-  const toggleTodo = (index) => {
+  const fetchCatImage = async (index) => {
+    try {
+      const response = await fetch('https://cataas.com/cat');
+      const blob = await response.blob();
+      const imageUrl = URL.createObjectURL(blob);
+
+      const newTodos = [...todos];
+      newTodos[index].catImage = imageUrl;
+      setTodos(newTodos);
+    } catch (error) {
+      console.error('Error fetching cat image:', error);
+    }
+  };
+
+  const toggleTodo = async (index) => {
     const newTodos = [...todos];
     newTodos[index].completed = !newTodos[index].completed;
     setTodos(newTodos);
+
+    await fetchCatImage(index);
   };
 
   const removeTodo = (index) => {
     const newTodos = [...todos];
     newTodos.splice(index, 1);
     setTodos(newTodos);
+  };
+
+  const openModal = async (index) => {
+    const currentTodo = todos[index];
+
+    // Check if the cat image is already fetched
+    if (!currentTodo.catImage) {
+      await fetchCatImage(index);
+    }
+
+    setSelectedTodo(currentTodo);
+  };
+
+  const closeModal = () => {
+    setSelectedTodo(null);
   };
 
   const handleKeyPress = (e) => {
@@ -63,23 +98,55 @@ const Todo = () => {
             className="list-group-item d-flex justify-content-between align-items-center"
           >
             {todo.text}
-            <button
-              className="btn btn-danger custom-close-button"
-              onClick={(e) => {
-                e.stopPropagation();
-                removeTodo(index);
-              }}
-              style={{
-                padding: '0.375rem 0.75rem',
-                marginLeft: '8px',
-                position: 'relative',
-              }}
-            >
-              <i className="fas fa-times"></i>
-            </button>
+            <div>
+              <button
+                className="btn btn-info mr-2"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  openModal(index);
+                }}
+              >
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </button>
+              <button
+                className="btn btn-danger custom-close-button"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  removeTodo(index);
+                }}
+                style={{
+                  padding: '0.375rem 0.75rem',
+                  marginLeft: '8px',
+                  position: 'relative',
+                }}
+              >
+                <i className="fas fa-times"></i>
+              </button>
+            </div>
           </li>
         ))}
       </ul>
+
+      {/* Modal */}
+      <Modal show={!!selectedTodo} onHide={closeModal} animation={true}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {selectedTodo && selectedTodo.text}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {selectedTodo && (
+            <>
+              <img src={selectedTodo.catImage} alt="Cat" style={{ maxWidth: '100%' }} />
+            </>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={closeModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
